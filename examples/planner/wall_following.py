9071af1a926bb356f1a9a0e4c85cb10e2f1836cb
Cho-Hyeongrae <77897596+Cho-Hyeongrae@users.noreply.github.com>
2021-12-22 09:34:06 +0900

    Update wall_following.py

--- examples/planner/wall_following.py
+++ examples/planner/wall_following.py
@@ -6,16 +6,16 @@ class WallPlanner:
 
     def plan(self, obs):
         ranges = obs['scans']
-        left = ranges[720]
+        left = ranges[720]  
         
 
-        if left > 2.1:
+        if left > 2.1:      # If you move away from the left wall, you will get closer to the left wall.
             steering_angle = 0.15
             speed = 2.0
-        elif left < 1.7:
+        elif left < 1.7:    # If you get too close to the left wall, you move to the right.
             steering_angle = -0.15
             speed = 2.0
-        else:
+        else: # When you keep a certain distance from the wall, you drive at high speed.
             steering_angle = 0.0
             speed = 7.0
             
